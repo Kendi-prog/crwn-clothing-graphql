df5882569c3575ff00a1c0eaf0f6684335d038a0
Kendi <muriungikendileila@gmail.com>
2025-01-14 22:28:47 +0300

    adding mutations

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery, useMutation } from '@apollo/client';
 
 import ProductCard from '../../components/product-card/product-card.component';
 import Spinner from "../../components/spinner/spinner.component";
@@ -11,18 +11,26 @@ import { CategoryContainer, Title } from './category.styles';
 const GET_CATEGORY = gql`
   query($title : string!) {
     getCollectionsByTitle(title: $title) {
-    id
-    title
-    item {
-      id
-      name
-      price
-      imageUrl
-      }
+    
     }
   
   }
 
+`
+
+const SET_CATEGORY = gql`
+  mutation($category : Category!) {
+    addCategory(category: $category) {
+      id
+      title
+      item {
+        id
+        name
+        price
+        imageUrl
+        }
+      }
+    }
 `
 
 const Category = () => {
@@ -32,6 +40,10 @@ const Category = () => {
       title: category
     }
   });
+
+  const [addCategory, {loading, error, data}] = useMutation(SET_CATEGORY);
+
+  addCategory({ variable: { category: categoryObject } })
 
   useEffect(() => {
     if(data) {
